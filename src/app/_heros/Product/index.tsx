66a4ec16d4cb7c60d2bf40c9d3ab19117def38f4
steven_savarin <srssdesing@gmail.com>
2024-02-23 00:11:30 +0100

    last configurations

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -1,71 +1,58 @@
-'use client'
+import React, { Fragment } from 'react'
 
-import React, { useEffect, useState } from 'react'
-
-import { Product } from '../../../payload/payload-types'
+import { Category, Product } from '../../../payload/payload-types'
+import { AddToCartButton } from '../../_components/AddToCartButton'
+import { Gutter } from '../../_components/Gutter'
+import { Media } from '../../_components/Media'
+import { Price } from '../../_components/Price'
 
 import classes from './index.module.scss'
 
-export const priceFromJSON = (priceJSON: string, quantity: number = 1, raw?: boolean): string => {
-  let price = ''
-
-  if (priceJSON) {
-    try {
-      const parsed = JSON.parse(priceJSON)?.data[0]
-      const priceValue = parsed.unit_amount * quantity
-      const priceType = parsed.type
-
-      if (raw) return priceValue.toString()
-
-      price = (priceValue / 100).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD', // TODO: use `parsed.currency`
-      })
-
-      if (priceType === 'recurring') {
-        price += `/${
-          parsed.recurring.interval_count > 1
-            ? `${parsed.recurring.interval_count} ${parsed.recurring.interval}`
-            : parsed.recurring.interval
-        }`
-      }
-    } catch (e) {
-      console.error(`Cannot parse priceJSON`) // eslint-disable-line no-console
-    }
-  }
-
-  return price
-}
-
-export const Price: React.FC<{
+export const ProductHero: React.FC<{
   product: Product
-  quantity?: number
-  button?: 'addToCart' | 'removeFromCart' | false
-}> = props => {
-  const { product, product: { priceJSON } = {}, button = 'addToCart', quantity } = props
-
-  const [price, setPrice] = useState<{
-    actualPrice: string
-    withQuantity: string
-  }>(() => ({
-    actualPrice: priceFromJSON(priceJSON),
-    withQuantity: priceFromJSON(priceJSON, quantity),
-  }))
-
-  useEffect(() => {
-    setPrice({
-      actualPrice: priceFromJSON(priceJSON),
-      withQuantity: priceFromJSON(priceJSON, quantity),
-    })
-  }, [priceJSON, quantity])
+}> = ({ product }) => {
+  const { title, categories, meta: { image: metaImage, description } = {} } = product
 
   return (
-    <div className={classes.actions}>
-      {typeof price?.actualPrice !== 'undefined' && price?.withQuantity !== '' && (
-        <div className={classes.price}>
-          <p>{price?.withQuantity}</p>
+    <Gutter className={classes.productHero}>
+      <div className={classes.mediaWrapper}>
+        {!metaImage && <div className={classes.placeholder}>No image</div>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </div>
+
+      <div className={classes.details}>
+        <h3 className={classes.title}>{title}</h3>
+
+        <div className={classes.categoryWrapper}>
+          <div className={classes.categories}>
+            {categories?.map((category, index) => {
+              const { title: categoryTitle } = category as Category
+
+              const titleToUse = categoryTitle || 'Generic'
+              const isLast = index === categories.length - 1
+
+              return (
+                <p key={index} className={classes.category}>
+                  {titleToUse} {!isLast && <Fragment>, &nbsp;</Fragment>}
+                  <span className={classes.separator}>|</span>
+                </p>
+              )
+            })}
+          </div>
+          <p className={classes.stock}> In stock</p>
         </div>
-      )}
-    </div>
+
+        <Price product={product} button={false} />
+
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
+        </div>
+
+        <AddToCartButton product={product} className={classes.addToCartButton} />
+      </div>
+    </Gutter>
   )
 }
--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,7 +1,7 @@
 export const inclusions = [
   {
     title: 'Free Shipping',
-    description: 'Free shipping for order above $150',
+    description: 'Free shipping',
     icon: '/assets/icons/shipping.svg',
   },
   {
