88f84750c813929347dc10483e313af36f131772
steven_savarin <srssdesing@gmail.com>
2024-02-22 20:10:49 +0100

    product details

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -1,94 +1,71 @@
-import React, { Fragment } from 'react'
-import Link from 'next/link'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 
 import { Product } from '../../../payload/payload-types'
-import { AddToCartButton } from '../../_components/AddToCartButton'
-import { Gutter } from '../../_components/Gutter'
-import { Media } from '../../_components/Media'
-import { Message } from '../../_components/Message'
-import { Price } from '../../_components/Price'
-import RichText from '../../_components/RichText'
 
 import classes from './index.module.scss'
 
-export const ProductHero: React.FC<{
+export const priceFromJSON = (priceJSON: string, quantity: number = 1, raw?: boolean): string => {
+  let price = ''
+
+  if (priceJSON) {
+    try {
+      const parsed = JSON.parse(priceJSON)?.data[0]
+      const priceValue = parsed.unit_amount * quantity
+      const priceType = parsed.type
+
+      if (raw) return priceValue.toString()
+
+      price = (priceValue / 100).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD', // TODO: use `parsed.currency`
+      })
+
+      if (priceType === 'recurring') {
+        price += `/${
+          parsed.recurring.interval_count > 1
+            ? `${parsed.recurring.interval_count} ${parsed.recurring.interval}`
+            : parsed.recurring.interval
+        }`
+      }
+    } catch (e) {
+      console.error(`Cannot parse priceJSON`) // eslint-disable-line no-console
+    }
+  }
+
+  return price
+}
+
+export const Price: React.FC<{
   product: Product
-}> = ({ product }) => {
-  const {
-    id,
-    stripeProductID,
-    title,
-    categories,
-    meta: { image: metaImage, description } = {},
-  } = product
+  quantity?: number
+  button?: 'addToCart' | 'removeFromCart' | false
+}> = props => {
+  const { product, product: { priceJSON } = {}, button = 'addToCart', quantity } = props
+
+  const [price, setPrice] = useState<{
+    actualPrice: string
+    withQuantity: string
+  }>(() => ({
+    actualPrice: priceFromJSON(priceJSON),
+    withQuantity: priceFromJSON(priceJSON, quantity),
+  }))
+
+  useEffect(() => {
+    setPrice({
+      actualPrice: priceFromJSON(priceJSON),
+      withQuantity: priceFromJSON(priceJSON, quantity),
+    })
+  }, [priceJSON, quantity])
 
   return (
-    <Fragment>
-      {!stripeProductID && (
-        <Gutter>
-          <Message
-            className={classes.warning}
-            warning={
-              <Fragment>
-                {'This product is not yet connected to Stripe. To link this product, '}
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}
-                >
-                  edit this product in the admin panel
-                </Link>
-                {'.'}
-              </Fragment>
-            }
-          />
-        </Gutter>
+    <div className={classes.actions}>
+      {typeof price?.actualPrice !== 'undefined' && price?.withQuantity !== '' && (
+        <div className={classes.price}>
+          <p>{price?.withQuantity}</p>
+        </div>
       )}
-      <Gutter className={classes.productHero}>
-        <div className={classes.content}>
-          <div className={classes.categories}>
-            {categories?.map((category, index) => {
-              if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
-
-                const titleToUse = categoryTitle || 'Untitled category'
-
-                const isLast = index === categories.length - 1
-
-                return (
-                  <Fragment key={index}>
-                    {titleToUse}
-                    {!isLast && <Fragment>, &nbsp;</Fragment>}
-                  </Fragment>
-                )
-              }
-
-              return null
-            })}
-          </div>
-          <h1 className={classes.title}>{title}</h1>
-          <div>
-            <p className={classes.description}>
-              {`${description ? `${description} ` : ''}To edit this product, `}
-              <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
-                navigate to the admin dashboard
-              </Link>
-              {'.'}
-            </p>
-          </div>
-          <Price product={product} button={false} />
-          <AddToCartButton product={product} className={classes.addToCartButton} />
-        </div>
-        <div className={classes.media}>
-          <div className={classes.mediaWrapper}>
-            {!metaImage && <div className={classes.placeholder}>No image</div>}
-            {metaImage && typeof metaImage !== 'string' && (
-              <Media imgClassName={classes.image} resource={metaImage} fill />
-            )}
-          </div>
-          {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-            <RichText content={metaImage.caption} className={classes.caption} />
-          )}
-        </div>
-      </Gutter>
-    </Fragment>
+    </div>
   )
 }
